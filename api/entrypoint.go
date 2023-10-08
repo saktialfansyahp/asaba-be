9883437fcee7f1347a0f74d46bccfa0661d21887
saktialfansyahp <saktialfansyahp@gmail.com>
2023-10-08 19:45:53 +0700

    add vercel config

--- api/entrypoint.go
+++ api/entrypoint.go
@@ -14,9 +14,22 @@ var (
 	app *gin.Engine
 )
 
+func corsMiddleware(c *gin.Context) {
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+
+	if c.Request.Method == "OPTIONS" {
+		c.AbortWithStatus(http.StatusOK)
+	} else {
+		c.Next()
+	}
+}
+
 func router(r *gin.RouterGroup){
 	models.ConnectDatabase()
 
+	r.Use(corsMiddleware)
 	r.GET("api/barang", controllers.Index)
 	r.POST("api/barang", controllers.Create)
 	r.POST("api/stok", controllers.Stok)
@@ -35,29 +48,7 @@ func init() {
 	})
 
 	r := app.Group("/")
-	r.Use(func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
-		if c.Request.Method == "OPTIONS" {
-			c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
-			c.JSON(http.StatusOK, gin.H{"message": "Preflight request successful"})
-			c.Abort()
-			return
-		}
-
-		if c.Request.Method == "POST" {
-			c.Writer.Header().Set("Access-Control-Allow-Methods", "POST")
-		} else if c.Request.Method == "GET" {
-			c.Writer.Header().Set("Access-Control-Allow-Methods", "GET")
-		} else if c.Request.Method == "PUT" {
-			c.Writer.Header().Set("Access-Control-Allow-Methods", "PUT")
-		} else if c.Request.Method == "DELETE" {
-			c.Writer.Header().Set("Access-Control-Allow-Methods", "DELETE")
-		}
-
-		c.Next()
-	})
 	router(r)
 }
 
