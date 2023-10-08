05bf40c353d88c8984ac67280e62df55cbdef995
saktialfansyahp <saktialfansyahp@gmail.com>
2023-10-08 20:16:00 +0700

    default app

--- models/setup.go
+++ models/setup.go
@@ -8,8 +8,7 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase(){
-	// dsn := "host=localhost user=postgres password=alfan12345 dbname=asaba port=5432"
-    dsn := "postgres://default:xBTaUne1kOj2@ep-shrill-base-66245357.ap-southeast-1.postgres.vercel-storage.com:5432/verceldb"
+	dsn := "host=localhost user=postgres password=alfan12345 dbname=asaba port=5432"
     db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
     
     if err !=nil {
