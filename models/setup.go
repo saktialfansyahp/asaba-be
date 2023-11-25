772a899453ca4aa643b1fc26050ca2796dc1c5e7
AinurRahmanAlfajri <76832688+AinurRahmanAlfajri@users.noreply.github.com>
2023-11-25 13:11:38 +0700

    Update setup.go

--- models/setup.go
+++ models/setup.go
@@ -8,14 +8,15 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase(){
-	dsn := "host=localhost user=postgres password=alfan12345 dbname=asaba port=5432"
+	dsn := "postgres://default:xBTaUne1kOj2@ep-shrill-base-66245357.ap-southeast-1.postgres.vercel-storage.com:5432/verceldb"
+    // dsn := "host=localhost user=postgres password=alfan12345 dbname=asaba port=5432"
     db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
     
     if err !=nil {
         panic("failed to connect database")
     }
 
-    db.AutoMigrate(&Barang{}, &Stok{})
+    // db.AutoMigrate(&Barang{}, &Stok{})
 
     DB = db
 }
